786811ec75ac3d431701a135824b853f5b963e03
MattiaToscano <mattia1997ct@gmail.com>
2025-05-14 13:41:27 +0200

    [UPD] Importato axios, bootstrap, creata costante per endpoint

--- react-post-form/src/App.jsx
+++ react-post-form/src/App.jsx
@@ -1,6 +1,12 @@
 // Hook
-import { useState } from 'react'
+import { useState } from 'react';
+//Importiamo Axios
+import { axios } from 'axios';
+//Importo Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css"
 
+//Creo costante endpoint
+const endpoint = 'https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts'
 
 
 function App() {
